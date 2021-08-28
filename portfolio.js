e559f8266287317590cad5d69fecf23d6918225a
Chelsey <chelsey.tschida@outlook.com>
2021-08-28 02:24:39 -0400

    footer dynamic date / cleaning up css

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,26 @@
-/* -------- NAV BAR --------*/
+/* ===== NAV BAR ===== */
 
-//nav bar changes color on scroll
+// nav bar changes color on scroll
 $(function () {
-    $(document).scroll(function () {
-      var $nav = $(".navbar");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
+  $(document).scroll(function () {
+    var $nav = $('.navbar')
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height())
+  })
 })
 
-//hides navbar dropdown on mobile once link is selected
+// hides navbar dropdown on mobile once link is selected
 $('.navbar-nav>.nav-link').on('click', function () {
-    $('.navbar-collapse').collapse('hide');
+  $('.navbar-collapse').collapse('hide')
 })
+
+/* ====== Footer ====== */
+
+{
+  /* <a class="copy-link scroll top text-muted" href="#"
+            >&copy Chelsey Tschida 2020</a> */
+}
+
+// const copyright = ''
+const date = new Date().getFullYear()
+const footerText = document.querySelector('.copy-link__js')
+footerText.textContent = `Chelsey Tschida ${date}`
